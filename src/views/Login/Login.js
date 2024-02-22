7ecd6bf4972dce55b927f4e53d1e7cc71798cef7
Srushti2411 <srushtigarad24@gmail.com>
2024-02-22 19:19:43 +0530

    Login page created

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -3,7 +3,12 @@ import './Login.css'
 function Login() {
   return (
     <div>
-      <h1 className='Main-Container'>login page</h1>
+      <form  className='form-container'><br/><br/><br/>
+        Enter Username : <input type='text' placeholder='Enter Your Email or Phone No.' className='input-box' required/><br/><br/><br/>
+        Enter Password : <input type='password' placeholder='Enter Password' className='input-box' required/><br/><br/><br/>
+        <input type='submit' value='Log in' className='login-btn'/><br/><br/><br/>
+        <p>Don't have account? <a href=''>Register Here</a></p>
+      </form>
     </div>
   )
 }
