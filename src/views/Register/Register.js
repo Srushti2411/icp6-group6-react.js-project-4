f62db70a11300129f13304489e239c0b64e855fc
Srushti2411 <srushtigarad24@gmail.com>
2024-02-23 10:36:38 +0530

    Updated Register and login page

--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,31 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Register.css';
 
 function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegistration = () => {
+    // Print data to console
+    console.log("Username:", username);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Confirm Password:", confirmPassword);
+
+    alert('Registration Successful !!');
+
+    setUsername('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+  };
+
   return (
     <div>
       <h1>Registration Form</h1>
-      <RegistrationForm />
+      <RegistrationForm
+        handleRegistration={handleRegistration}
+        username={username}
+        setUsername={setUsername}
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        confirmPassword={confirmPassword}
+        setConfirmPassword={setConfirmPassword}
+      />
     </div>
   );
 }
 
-const RegistrationForm = () => {
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    };
-  
-    return (
-      <form onSubmit={handleSubmit} className='Register-form-container'>
-        Enter username : <input type="text" name="username" placeholder="Username" className='input-box' /><br/><br/>
-        Enter email :    <input type="email" name="email" placeholder="Email" className='input-box' /><br/><br/>
-        Enter password : <input type="password" name="password" placeholder="Password" className='input-box'/><br/><br/>
-        Confirm password : <input type="password" name="confirmPassword" placeholder="Confirm Password" className='input-box' /><br/><br/>
-        <button type="submit" className='login-btn'>Register</button>
-        <p>Already have an account? <a href=''>Login here</a></p>
-      </form>
-    );
+const RegistrationForm = ({
+  handleRegistration,
+  username,
+  setUsername,
+  email,
+  setEmail,
+  password,
+  setPassword,
+  confirmPassword,
+  setConfirmPassword
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleRegistration();
   };
 
-export default Register
+  return (
+    <form onSubmit={handleSubmit} className='Register-form-container'>
+      Enter username : <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        className='input-box'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      /><br/><br/>
+      Enter email :    <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        className='input-box'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      /><br/><br/>
+      Enter password : <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        className='input-box'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      /><br/><br/>
+      Confirm password : <input
+        type="password"
+        name="confirmPassword"
+        placeholder="Confirm Password"
+        className='input-box'
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        required
+      /><br/><br/>
+      <button type="submit" className='login-btn'>Register</button>
+      <p>Already have an account? <Link to='/login'>Login Here</Link></p>
+    </form>
+  );
+};
 
+export default Register;
